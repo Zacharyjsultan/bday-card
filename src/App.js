b7a22a42cb46b4ecf6a140bfd983d71381ff0afa
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2023-03-08 11:34:10 -0800

    Card

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Card from "./Card";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
